0d3a34e5b3f6798484d7c0426c69d10e65db3b18
tusharmauryaa <101079264+tusharmauryaa@users.noreply.github.com>
2022-03-07 17:27:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 
 echo "
 *********** STARTING DEPLOY ***********
-
-DAISYX v2 -Base Aiogram
-(C) 2020-2021 by @TEAMDAISYX
-Support Chat is @DAISYSUPPORT_OFFICIAL.
+URVASHI v2 -Base Aiogram
+(C) 2020-2021 by @tusharmaurya
+Support Chat is @quicktriviaa.
 
 ***************************************
 "
@@ -30,15 +29,15 @@ ech_final () {
     echo "
     
 =++---------------------------------------------++=
-DAISYX. Deployed Successfully
+URVASHI. Deployed Successfully
 
  ***************************
  * |D| |A| |I| |S| |Y| |X| *
  ******************* v2.0 ** 
 
 Thanks for deploying DaisyX
-(C) 2020-2021 by @TEAMDAISYX
-Support Chat is @DAISYSUPPORT_OFFICIAL.
+(C) 2020-2021 by @tusharmaurya
+Support Chat is @quicktriviaaa.
 =++---------------------------------------------++=
                        Greetings from dev team :)
     "
